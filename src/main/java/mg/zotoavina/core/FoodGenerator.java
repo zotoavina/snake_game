8e4d9f8767a94e3fd276be7366c815aeda2425a2
ZOTOAVINA\ASUS <zotoavinanantenaina@gmail.com>
2024-02-07 06:42:55 +0100

    Fix methods for food generation

--- src/main/java/mg/zotoavina/core/FoodGenerator.java
+++ src/main/java/mg/zotoavina/core/FoodGenerator.java
@@ -2,24 +2,27 @@ package mg.zotoavina.core;
 
 import java.awt.*;
 import java.util.List;
+import java.util.Random;
 
 public final class FoodGenerator {
 
     private static final List<Color> colors;
+    private static final Random random;
 
     static {
         colors = List.of(Color.BLACK, Color.GRAY,
                 Color.LIGHT_GRAY, Color.BLUE, Color.red, Color.orange, Color.CYAN);
+        random = new Random();
     }
 
     private FoodGenerator() {
     }
 
     public static Food generateFood(int minX, int minY, int maxX, int maxY, int bound) {
-        int x = (int) (Math.random() * maxX + minX) - bound;
-        int y = (int) (Math.random() * maxY + minY) - bound;
 
-        int colorIndex = (int) (Math.random() * colors.size());
+        int x = random.nextInt(maxX - minX - bound);
+        int y = random.nextInt(maxY - minY - bound);
+        int colorIndex = random.nextInt(colors.size());
 
         return new Food((x / bound) * bound, (y / bound) * bound, colors.get(colorIndex), Shape.RECT);
     }
